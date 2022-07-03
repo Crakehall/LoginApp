338d658e34964fc8258b34365af0261053c068fb
Oleg Sosnitskiy <orlovolegpr@gmail.com>
2022-07-03 22:51:31 +0300

    Added constraints at WelcomeView

--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -13,7 +13,8 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        welcomeLabel.text = "Welcome, \(userName ?? "")"
+        welcomeLabel.text = "Welcome, \(userName ?? "")!"
     }
 
 }
+
